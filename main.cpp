5dc612bd3a402a8b7d08ec7bc54f5e65c89ccab9
SoDebug <1363351571@qq.com>
2024-06-22 20:24:26 +0800

    test: 初步实验 宏 能否用c++生成

--- main.cpp
+++ main.cpp
@@ -1,16 +1,53 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+# define user_input_filename false
+
 using namespace std;
 
 int main() {
-    //澹版槑涓�涓枃浠惰緭鍑虹被
-    ifstream target;
-    // ifstream鐨刼pen()鏂规硶鐢ㄤ簬鎵撳紑鏂囦欢
-    target.open("Fields.bas");
-    if (!target)
-        cout << "cannot open target files :( " << endl;
-    else
-        cout << "file can be read :)" << endl;
-        target.close();
+    string file_name;
+    string Header;
+
+    if (user_input_filename){
+        // 提示用户输入文件名
+        cout << "请键入文件名" << endl;
+        cin >> file_name;
+    }
+    else{
+        file_name = "script";
+    }
+    file_name = file_name + ".bas";
+    cout << file_name <<endl;
+
+    // 创建并打开文件
+    ofstream target(file_name.c_str());
+
+    // 检查文件是否成功打开
+    if (!target) {
+        cout << "无法打开文件 " << file_name << " :(" << endl;
+    } else {
+        cout << "文件" << file_name << " 已创建 :)" << endl;
+        target << "Attribute VB_Name = \"NewMacros\"" << endl;
+        target << "Sub AutoDoc()" << endl;
+        target << "\'" << endl;
+        target << "\'  AutoDoc 宏" << endl;
+        target << "\'" << endl;
+        target << "    Selection.EndKey Unit:=wdStory" << endl;
+        target << "    Selection.InsertBreak Type:=wdSectionBreakNextPage" << endl;
+        target << "    Selection.Fields.Add Range:=Selection.Range, Type:=wdFieldEmpty, PreserveFormatting:=False" << endl;
+        target << "    Selection.TypeText Text:=\"SET 项目代号 \"\"\"\"\"" << endl;
+        target << "    Selection.MoveLeft Unit:=wdCharacter, Count:=1" << endl;
+        target << "    Selection.TypeText Text:=\"1563\"" << endl;
+        target << "    Selection.MoveRight Unit:=wdCharacter, Count:=3" << endl;
+        target << "    Selection.TypeParagraph" << endl;
+        target << "    Selection.Fields.Add Range:=Selection.Range, Type:=wdFieldEmpty, PreserveFormatting:=False" << endl;
+        target << "    Selection.TypeText Text:=\"REF 项目代号\"" << endl;
+        target << "    Selection.WholeStory" << endl;
+        target << "    Selection.Fields.Update" << endl;
+        target << "End Sub" << endl;
+        target.close();  // 关闭文件
+    }
+
     return 0;
 }
